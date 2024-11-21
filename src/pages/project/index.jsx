0222ff1b90c4d5f9f2cbd33049f1be9b7ed0c665
Gill Guillermin <guillerg680@gmail.com>
2024-11-21 16:00:52 +0100

    ouverture page projet en scrolltop

--- src/pages/project/index.jsx
+++ src/pages/project/index.jsx
@@ -1,4 +1,4 @@
-
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import projectsData from '../../data/projects-data.json'; 
 
@@ -6,6 +6,12 @@ const Project = () => {
     const { id } = useParams(); // Récupération de l'id depuis l'URL
     const project = projectsData.find((project) => project.id === id); // Recherche du projet correspondant via l'id
   
+    useEffect(() => {
+        // Défilement vers le haut lorsque le composant est monté
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, []);
+
+
     return (
         <main>
             <section className='project'>
